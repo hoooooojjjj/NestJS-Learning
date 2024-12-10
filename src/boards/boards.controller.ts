098ac4b6b9143ea3f48888495e28741ee907fddd
sono <rhj080471@gmail.com>
2024-12-11 02:57:30 +0900

    Practice : 간단하게 board를 반환하고 생성, 삭제하는 코드 구현해보기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Req } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Request } from 'express';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
+
+  @Get()
+  getAllBoards() {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Get('/:id')
+  getBoardById(@Req() request: Request) {
+    return this.boardsService.getBoardById(Number(request.params.id));
+  }
+
+  @Post('/:id/columns')
+  createColumn(@Req() request: Request, @Body() req: any) {
+    return this.boardsService.createColumn(
+      Number(request.params.id),
+      request.body,
+    );
+  }
+
+  @Delete('/:id/columns/:column_id')
+  deleteColumn(@Req() request: Request) {
+    return this.boardsService.deleteColumn(Number(request.params.column_id));
+  }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  board = {
+    id: 1,
+    title: 'First board',
+    columns: [
+      {
+        id: 1,
+        title: 'Column 1',
+        order: 1,
+      },
+      {
+        id: 2,
+        title: 'Column 2',
+        order: 2,
+      },
+    ],
+  };
+
+  getAllBoards(): any {
+    return this.board;
+  }
+
+  getBoardById(id: number): any {
+    return this.board.id === id ? this.board : null;
+  }
+
+  createColumn(id: any, column: any): any {
+    this.board.columns.push(column);
+    return column;
+  }
+
+  deleteColumn(column_id: any): any {
+    const deletedColumn = this.board.columns.find(
+      (column) => column.id === column_id,
+    );
+    this.board.columns = this.board.columns.filter(
+      (column) => column.id !== column_id,
+    );
+    return deletedColumn;
+  }
+}
