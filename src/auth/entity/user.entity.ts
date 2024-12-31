1d989a429f4f9a883fea84af61451f50801b7bea
sono <sono7@iruri-labs.com>
2024-12-31 16:34:17 +0900

    Learning : 유저 이름에 유니크한 값 주기
    엔티티 레벨에서 @Unique 데코레이터를 사용, 데이터를 처리할 때 에러 코드를 커스터마이징하기 위해서 try catch 문을 사용

--- src/auth/entity/user.entity.ts
+++ src/auth/entity/user.entity.ts
@@ -1,13 +1,21 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 
 @Entity()
+// 엔티티 레벨에서 @Unique 데코레이터를 사용하여 유니크한 값을 지정할 수 있음
+@Unique(['username'])
 export class UserEntity extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ nullable: false }) // username이 필수값임을 명시
   username: string;
 
-  @Column()
+  @Column({ nullable: false }) // password도 필수값으로 설정
   password: string;
 }
--- src/auth/repository/user.repository.ts
+++ src/auth/repository/user.repository.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { UserEntity } from '../entity/user.entity';
 import { AuthCredentialDto } from '../dto/authCredentailDto';
@@ -13,13 +17,22 @@ export class UserRepository extends Repository<UserEntity> {
     const { username, password } = authCredentialDto;
 
     // 새로운 유저 생성
-    const newUser = await this.create({
+    const newUser = this.create({
       username,
       password,
     });
 
-    // 유저 생성 저장
-    await this.save(newUser);
+    // 데이터를 처리할 때 에러 코드를 커스터마이징하기 위해선 try catch 문을 사용해야 함
+    try {
+      // 유저 생성 저장
+      await this.save(newUser);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('이미 존재하는 아이디입니다');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
 
     return newUser;
   }
