f62c9f93c7b9a2ff614006aeedd11ff4ade8eeb6
ryuhojun <rhj080471@gmail.com>
2025-01-04 00:50:40 +0900

    Learning : 다른 모듈에서 AuthModule 자체를 import 해주면 인증 로직을 처리할 수 있음

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -4,28 +4,11 @@ import { BoardsService } from './boards.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardEntity } from './board.entity';
 import { BoardRepository } from './board.repository';
-import { JwtStrategy } from 'src/auth/jwt.strategy';
-import { PassportModule } from '@nestjs/passport';
-import { UserEntity } from 'src/auth/entity/user.entity';
-import { JwtModule } from '@nestjs/jwt';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   // board module에 BoardEntity import
-  imports: [
-    TypeOrmModule.forFeature([BoardEntity]),
-    TypeOrmModule.forFeature([UserEntity]),
-    // Jwt를 사용하기 위해서 Auth 모듈에 Jwt 모듈 등록
-    JwtModule.register({
-      secret: 'Secret1234',
-      signOptions: {
-        expiresIn: 360 * 60,
-      },
-    }),
-    // Passport를 사용하기 위해서 Auth 모듈에 Passport 모듈 등록
-    PassportModule.register({
-      defaultStrategy: 'jwt',
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([BoardEntity]), AuthModule],
   controllers: [BoardsController],
   // BoardRepository provider 등록
   providers: [BoardsService, BoardRepository],
