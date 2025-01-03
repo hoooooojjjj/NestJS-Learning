f6bc1fc457a9dcf3a50ac1127134b0e9c416073b
ryuhojun <rhj080471@gmail.com>
2025-01-04 01:03:48 +0900

    Learning : 유저와 게시물 간 관계 설정

--- src/auth/entity/user.entity.ts
+++ src/auth/entity/user.entity.ts
@@ -1,7 +1,9 @@
+import { BoardEntity } from 'src/boards/board.entity';
 import {
   BaseEntity,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   Unique,
 } from 'typeorm';
@@ -18,4 +20,12 @@ export class UserEntity extends BaseEntity {
 
   @Column({ nullable: false }) // password도 필수값으로 설정
   password: string;
+
+  // 유저와 게시판 엔티티 간의 관계를 설정 -> 유저가 여러 개의 게시판을 가질 수 있음을 명시
+  // type: 게시판 엔티티를 가리키는 타입
+  // board: 게시판 엔티티를 가리키는 변수 -> BoardEntity에서 user를 어떻게 가져오는지 지정
+  // eager: true는 유저를 가져올 때 게시판도 함께 가져오도록 설정
+  // eager: false는 유저를 가져올 때 게시판을 가져오지 않도록 설정
+  @OneToMany((type) => BoardEntity, (board) => board.user, { eager: true })
+  boards: BoardEntity[];
 }
--- src/boards/board.entity.ts
+++ src/boards/board.entity.ts
@@ -1,5 +1,12 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { BoardsStatus } from './boards-status-enum';
+import { UserEntity } from 'src/auth/entity/user.entity';
 
 // typeorm을 통해서 데이터베이스 테이블을 만들기 위해선 entity를 정의해야 한다.
 // 아래와 같이 BaseEntity를 상속 받아서 BoardEntity라는 클래스를 만들고 멤버로 각 테이블의 열을 만들어줄 수 있다.
@@ -17,4 +24,11 @@ export class BoardEntity extends BaseEntity {
 
   @Column({ default: BoardsStatus.PUBLIC })
   status: BoardsStatus;
+
+  // 유저와 게시판 엔티티 간의 관계를 설정 -> 게시판은 하나의 유저만 가질 수 있음을 명시
+  // type: 유저 엔티티를 가리키는 타입
+  // user: 유저 엔티티를 가리키는 변수 -> UserEntity에서 boards를 어떻게 가져오는지 지정
+  // eager: false는 유저를 가져올 때 게시판을 가져오지 않도록 설정
+  @ManyToOne((type) => UserEntity, (user) => user.boards, { eager: false })
+  user: UserEntity;
 }
